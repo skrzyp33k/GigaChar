ec40035a7404fbe1fca2857ce15fa44f6620a9b1
jszczerbinsky <jszczerbinsky@gmail.com>
2022-11-06 18:17:42 +0000

    Most important functions and part of documentation

--- gigachar.c
+++ gigachar.c
@@ -1,10 +1,197 @@
+#include <stdio.h>
+#include <string.h>
 #include "gigachar.h"
 
-void print(gigachar c)
+void putgigachar(gigachar c)
 {
     char value = (char)(c & GC_CHAR);
 
     if((c & GC_BOLD) == GC_BOLD) printf("\e[1m");
+    if((c & GC_DIM) == GC_DIM) printf("\e[2m");
+    if((c & GC_ITALIC) == GC_ITALIC) printf("\e[3m");
+    if((c & GC_UNDERLINE) == GC_UNDERLINE) printf("\e[4m");
+    if((c & GC_BLINKING) == GC_BLINKING) printf("\e[5m");
+    if((c & GC_INVERSE) == GC_INVERSE) printf("\e[7m");
+    if((c & GC_HIDDEN) == GC_HIDDEN) printf("\e[8m");
+    if((c & GC_STRIKETROUGH) == GC_STRIKETROUGH) printf("\e[9m");
     putchar(c);
     printf("\e[0m");
+}
+
+void printgigastring(gigachar* gigastring)
+{
+    gigachar *ptr = gigastring;
+    while(*ptr != GC_GIGANULL)
+    {
+        putgigachar(*ptr);
+        ptr++;
+    }
+}
+
+void togigastring(gigachar *buff, size_t buff_length, const char* str, gigachar flags)
+{
+    int max = strlen(str);
+
+    if(buff_length < max)
+    {
+        max = buff_length;
+    }
+
+    for(int i = 0; i < max; i++)
+    {
+        buff[i] = str[i] | flags;
+    }
+}
+
+size_t gigastrlen(const gigachar *str)
+{
+    int count;
+    
+    gigachar *ptr = str;
+    while(*ptr != GC_GIGANULL)
+    {
+        ptr++;
+    }
+
+    return (ptr-str)/sizeof(gigachar);
+}
+
+void gigastrncat(gigachar* destination, const gigachar* source, size_t num)
+{
+    size_t pos = 0;
+
+    gigachar *ptr = destination+gigastrlen(destination)*sizeof(gigachar);
+    gigachar *src_ptr = source;
+
+    while(pos < num && *src_ptr)
+    {
+        *ptr = *src_ptr;
+        ptr++;
+        src_ptr++;
+        pos++;
+    }
+}
+
+void gigastrcat(gigachar* destination, const gigachar* source)
+{
+    gigachar *ptr = destination+gigastrlen(destination)*sizeof(gigachar);
+    gigachar *src_ptr = source;
+
+    while(*src_ptr)
+    {
+        *ptr = *src_ptr;
+        ptr++;
+        src_ptr++;
+    }
+}
+
+void gigastrncpy(gigachar* destination, const gigachar* source, size_t num)
+{
+    size_t pos = 0;
+
+    gigachar *ptr = destination;
+    gigachar *src_ptr = source;
+
+    while(pos < num && *src_ptr)
+    {
+        *ptr = *src_ptr;
+        ptr++;
+        src_ptr++;
+        pos++;
+    }
+
+    if(src_ptr < source+num*sizeof(gigachar))
+        *ptr = '\0';
+}
+
+void gigastrcpy(gigachar* destination, const gigachar* source)
+{
+    gigachar *ptr = destination-1;
+    gigachar *src_ptr = source-1;
+
+    do
+    {
+        ptr++;
+        src_ptr++;
+        *ptr = *src_ptr;
+    }
+    while(*src_ptr);
+}
+
+int gigastrcmp(const gigachar* str1, const gigachar* str2)
+{
+    const gigachar *ptr1 = str1;
+    const gigachar *ptr2 = str2;
+
+    while(*ptr1 && *ptr2)
+    {
+        if((*ptr1 & GC_CHAR) != (*ptr2 & GC_CHAR))
+            return *ptr2 & GC_CHAR - *ptr1 & GC_CHAR;
+
+        ptr1++;
+        ptr2++;
+    }
+
+    return 0;
+}
+
+int gigastrcmpfull(const gigachar* str1, const gigachar *str2)
+{
+    const gigachar *ptr1 = str1;
+    const gigachar *ptr2 = str2;
+
+    while(*ptr1 && *ptr2)
+    {
+        if(*ptr1 != *ptr2)
+            return *ptr2 - *ptr1;
+
+        ptr1++;
+        ptr2++;
+    }
+
+    return 0;
+}
+
+int gigastrncmp(const gigachar* str1, const gigachar *str2, size_t num)
+{
+    size_t pos = 0;
+
+    const gigachar *ptr1 = str1;
+    const gigachar *ptr2 = str2;
+
+    while(pos < num && *ptr1 && *ptr2)
+    {
+        if((*ptr1 & GC_CHAR) != (*ptr2 & GC_CHAR))
+            return *ptr2 & GC_CHAR - *ptr1 & GC_CHAR;
+
+        ptr1++;
+        ptr2++;
+        pos++;
+    }
+
+    return 0;
+}
+int gigastrncmpfull(const gigachar* str1, const gigachar *str2, size_t num)
+{
+    size_t pos = 0;
+
+    const gigachar *ptr1 = str1;
+    const gigachar *ptr2 = str2;
+
+    while(pos < num && *ptr1 && *ptr2)
+    {
+        if(*ptr1 != *ptr2)
+            return *ptr2 - *ptr1;
+
+        ptr1++;
+        ptr2++;
+        pos++;
+    }
+
+    return 0;
+}
+
+char tonormalchar(gigachar gc)
+{
+    return (char)(gc & GC_CHAR);
 }
--- gigachar.h
+++ gigachar.h
@@ -1,29 +1,97 @@
+//*
+  *
+  *
+  */
+
 #ifndef _GIGACHAR_H_
 #define _GIGACHAR_H_
 
-typedef int gigachar;
+typedef unsigned short gigachar;
 
-#define GC_BOLD 0b0000000100000000
-#define GC_DIM  0b0000001000000000
-#define GC_ITALIC 0b0000010000000000
-#define GC_UNDERLINE 0b0000100000000000
-#define GC_BLINKING 0b0001000000000000
-#define GC_INVERSE 0b0010000000000000
-#define GC_HIDDEN 0b0100000000000000
-#define GC_STRIKETROUGH 0b1000000000000000
+#define GC_BOLD 0b0000000100000000 ///<
+#define GC_DIM  0b0000001000000000 ///<
+#define GC_ITALIC 0b0000010000000000 ///<
+#define GC_UNDERLINE 0b0000100000000000 ///<
+#define GC_BLINKING 0b0001000000000000 ///<
+#define GC_INVERSE 0b0010000000000000 ///<
+#define GC_HIDDEN 0b0100000000000000 ///<
+#define GC_STRIKETROUGH 0b1000000000000000 ///<
 
-#define GC_CHAR 0b0000000011111111
+#define GC_GIGANULL 0 ///<
 
-/*
-ESC[1m	ESC[22m	set bold mode.
-ESC[2m	ESC[22m	set dim/faint mode.
-ESC[3m	ESC[23m	set italic mode.
-ESC[4m	ESC[24m	set underline mode.
-ESC[5m	ESC[25m	set blinking mode
-ESC[7m	ESC[27m	set inverse/reverse mode
-ESC[8m	ESC[28m	set hidden/invisible mode
-ESC[9m	ESC[29m	set strikethrough mode.
+#define GC_CHAR 0b0000000011111111 ///<
+/**
+ * @brief Prints gigachar on console output
+ * 
+ * @param c Gigachar to print
+ */
+void putgigachar(gigachar c);
 
-*/
+/**
+ * @brief Prints gigastring on console output
+ * 
+ * @param gigastring Gigastring to print
+ */
+void printgigastring(gigachar* gigastring);
+
+
+/**
+ * @brief Creates gigachars array
+ * 
+ * @param buff 
+ * @param buff_length 
+ * @param str 
+ * @param flags 
+ */
+void togigastring(gigachar *buff, size_t buff_length, const char* str, gigachar flags);
+
+/**
+ * @brief Copies one gigastring to another
+ * 
+ * @param destination Gigastring destination
+ * @param source Gigastring to copy from
+ */
+void gigastrcpy (gigachar* destination, const gigachar* source);
+
+/**
+ * @brief Copies n gigachars from one gigastring to another
+ * 
+ * @param destination Gigastring destination
+ * @param source Gigastring to copy from
+ * @param num Number of chars
+ */
+void gigastrncpy(gigachar* destination, const gigachar* source, size_t num);
+
+/**
+ * @brief Concatenates one gigastring to another
+ * 
+ * @param destination Gigastring destination
+ * @param source Gigastring to concatenate from
+ */
+void gigastrcat (gigachar* destination, const gigachar* source);
+
+/**
+ * @brief Concatenates n chars of one gigastring to another
+ * 
+ * @param destination Gigastring destination
+ * @param source Gigastring to concatenate from
+ * @param num Number of chars
+ */
+void gigastrncat(gigachar* destination, const gigachar* source, size_t num);
+
+/**
+ * @brief Returns gigastring length in gigachars
+ * 
+ * @param str Gigastring to get length from
+ * @return size_t Number of gigachars
+ */
+size_t gigastrlen(const gigachar* str);
+
+int gigastrcmp      (const gigachar* str1, const gigachar *str2);
+int gigastrcmpfull  (const gigachar* str1, const gigachar *str2);
+int gigastrncmp     (const gigachar* str1, const gigachar *str2, size_t num);
+int gigastrncmpfull (const gigachar* str1, const gigachar *str2, size_t num);
+
+char tonormalchar(gigachar gc);
 
 #endif
