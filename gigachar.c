89843d1acf970d1064dd5f36b013e1d2b8c90b42
Kacper Skrzypek <skrzyp33k@protonmail.com>
2022-11-06 16:42:12 +0100

    push dla czikena

--- gigachar.c
+++ gigachar.c
@@ -1,1 +1,10 @@
 #include "gigachar.h"
+
+void print(gigachar c)
+{
+    char value = (char)(c & GC_CHAR);
+
+    if((c & GC_BOLD) == GC_BOLD) printf("\e[1m");
+    putchar(c);
+    printf("\e[0m");
+}
